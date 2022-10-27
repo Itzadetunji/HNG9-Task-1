46c3f9040b8220f4a5da0d19db978138df83003c
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-10-27 19:52:05 +0100

    Chore: Added port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
-const port = 3000;
 const logger = require("morgan");
+let port = process.env.PORT || 3000;
 
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
