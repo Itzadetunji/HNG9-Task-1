f7b7f168ff47a85f8de18cf0774b986e2e8ce91f
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-11-05 13:38:45 +0100

    Chore: Made progress on stage 2 task

--- controller.js
+++ controller.js
@@ -1,5 +1,38 @@
 const handleOperation = (req, res) => {
-	console.log(1);
+	var result;
+	var { x, y } = req.body;
+	const { operation_type } = req.body;
+	if (!parseInt(x) || !parseInt(y)) {
+		return res
+			.status(400)
+			.json({ Error: "The input provided are not integers" });
+	} else {
+		x = parseInt(x);
+		y = parseInt(y);
+	}
+	const operators = {
+		ADDITION: ["addition", "add", "+", "sum"],
+		SUBTRACTION: ["subtraction", "subtract", "-", "minus"],
+		MULTIPLICATION: ["multiplication", "multiply", "x", "*", "times"],
+	};
+	const addOperation = operators.ADDITION.map((add) => add);
+	console.log(addOperation);
+	switch (operation_type.trim().toLowerCase()) {
+		case operators.ADDITION:
+			result = x + y;
+			break;
+		case operators.SUBTRACTION:
+			result = x - y;
+			break;
+		case operators.MULTIPLICATION:
+			result = x * y;
+			break;
+		default:
+			result = "I cannot do that calculation";
+	}
+	return res
+		.status(200)
+		.json({ slackUsername: "itzadetunji", result, operation_type });
 };
 
 module.exports = {
--- index.js
+++ index.js
@@ -2,29 +2,24 @@ const express = require("express");
 const app = express();
 const logger = require("morgan");
 const cors = require("cors");
+const body = (bodyParser = require("body-parser"));
 const port = process.env.PORT || 3000;
 
 const { handleOperation } = require("./controller");
 
-app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`);
-});
-
 app.use(logger("dev"));
-app.use(express.urlencoded({ extended: true }));
 app.use(
 	cors({
 		origin: "*",
 	})
 );
 
-app.get("/", (req, res) => {
-	res.json({
-		slackUsername: "itzadetunji",
-		backend: true,
-		age: 17,
-		bio: "I am a web developer trying his best :) to become a better person and transform the world",
-	});
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+// app.post("/stage2", handleOperation);
 app.post("/stage2", handleOperation);
+
+app.listen(port, () => {
+	console.log(`Stage 2 task listening on port ${port}`);
+});
